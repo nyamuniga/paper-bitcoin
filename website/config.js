494f0788ab02b64d64b6a62c08d437cd6e9b7de7
nyamuniga <knyamuniga16@gmail.com>
2026-07-23 11:08:31 +0200

    fix

--- website/config.js
+++ website/config.js
@@ -1,5 +1,4 @@
 window.ENV = {
-
     // Update this link whenever there is a new release.
     // The HTML will automatically extract the version number (e.g., v0.1.3) from this URL.
     APK_LINK: "https://github.com/nyamuniga/paper-bitcoin/releases/download/v0.1.3/bitnotes-android-wallet-v0.1.3.apk"
